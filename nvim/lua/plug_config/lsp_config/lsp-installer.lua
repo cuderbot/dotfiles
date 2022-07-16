2f547f4830be78fa2ca117bd3e659381b21634d5
Cuderbot <ja.becerraa@gmail.com>
2022-07-16 11:11:56 -0400

    add config for lsp installer

---
+++ nvim/lua/plug_config/lsp_config/lsp-installer.lua
@@ -0,0 +1,14 @@
+local lspInstaller = require("nvim-lsp-installer")
+
+lspInstaller.setup({
+    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+    ui = {
+        -- Whether to automatically check for outdated servers when opening the UI window.
+        check_outdated_servers_on_open = true,
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
+  })
