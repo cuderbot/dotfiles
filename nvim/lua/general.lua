2e7029e27d536ee011599d5e8009b384ff2e980b
Cuderbot <ja.becerraa@gmail.com>
2022-07-19 23:36:19 -0400

    update undodir path

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -24,7 +24,7 @@ vim.opt['laststatus'] = 3
 -- Persistent undo
 -- mkdir $HOME/.vim/undo
 vim.opt['undofile'] = true
-vim.opt['undodir'] = os.getenv( 'HOME' )..'/.cache/nvim/undo'
+vim.opt['undodir'] = os.getenv( 'HOME' )..'/.config/nvim/undodir/'
 vim.opt['undolevels'] = 1000
 vim.opt['undoreload'] = 10000
 
