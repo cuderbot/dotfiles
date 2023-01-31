3bc4cbe5c823927cc3958f10626dd8867b3d273b
Cuderbot <ja.becerraa@gmail.com>
2023-01-31 13:28:44 -0300

    feat: add gitsigns config

---
+++ nvim/lua/new/plugins/git.lua
@@ -0,0 +1,8 @@
+-- git integration
+return {
+  {
+    -- show line modifications on left hand side
+    'lewis6991/gitsigns.nvim',
+    config = true,  -- this is equal to function() require('gitsigns').setup() end
+  },
+}
