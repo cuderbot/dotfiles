31b39fa739b83de7887c4651e618408ee9dca26a
Cuderbot <ja.becerraa@gmail.com>
2023-01-31 15:01:07 -0300

    style: formatting syntax-highlighting config

--- nvim/lua/new/plugins/syntax-highlighting.lua
+++ nvim/lua/new/plugins/syntax-highlighting.lua
@@ -1,4 +1,27 @@
 return {
+  {
+    -- autoclose parens, brackets, quotes, etc...
+    'windwp/nvim-autopairs',
+    config = function()
+      local autopairs = require('nvim-autopairs')
+      autopairs.setup({
+        check_ts = true, -- enable treesitter
+        ts_config = {
+          lua = { 'string' }, -- don't add pairs in lua string treesitter nodes
+          javascript = { 'template_string' }, -- don't add pairs in javascript template_strings treesitter nodes 
+          java = false, -- don't check treesitter on java
+        }
+      })
+    end
+  },
+  {
+    -- autoclose tags
+    'windwp/nvim-ts-autotag',
+    dependencies = {
+      'nvim-treesitter'
+    }
+  },
+  {
   -- syntax highlighting
   'nvim-treesitter/nvim-treesitter',
   build = ":TSUpdate",
@@ -6,9 +29,7 @@ return {
     -- configure treesitter
     require('nvim-treesitter.configs').setup({
       -- enable syntax highlighting
-      highlighting = {
-        enable = true,
-      },
+      highlighting = { enable = true },
       -- enable indentation 
       indent = { enable = true },
       -- enable autotagging (w / nvim-ts-autotag plugin)
@@ -35,4 +56,5 @@ return {
       auto_install = true,
     })
   end
+  }
 }
