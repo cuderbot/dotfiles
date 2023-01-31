94167134f9e20686487bc3a576ba9550e17c5ce6
Cuderbot <ja.becerraa@gmail.com>
2023-01-31 18:13:32 -0300

    feat: add mind a note plugin

---
+++ nvim/lua/new/plugins/notes.lua
@@ -0,0 +1,12 @@
+return {
+  -- note taking and task workflows plugin
+  'phaazon/mind.nvim',
+  branch = "v2.2",
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  cmd = { "MindOpenMain", "MindOpenProject" },
+  keys = {
+    { '<leader>no', ':MindOpenMain<cr>' },
+    { '<leader>nc', ':MindClose<cr>' }, 
+  },
+  config = true
+}
