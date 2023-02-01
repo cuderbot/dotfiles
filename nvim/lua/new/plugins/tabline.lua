dec30620343666cbafe13586d78cb23522589350
Cuderbot <ja.becerraa@gmail.com>
2023-02-01 11:47:45 -0300

    feat: add a tab plugin

---
+++ nvim/lua/new/plugins/tabline.lua
@@ -0,0 +1,11 @@
+local M = {
+  -- buffer line (with tabpage integration)
+  'akinsho/bufferline.nvim',
+  dependencies = {
+    -- vs-code icons 
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = true,
+}
+
+return M
